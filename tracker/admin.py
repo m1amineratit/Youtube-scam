ec881675386abe8c76f382ba4686c247a91219fa
Hcode7 <ratitamine3@gmail.com>
2025-05-13 22:24:19 +0100

    dd

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Image, Location, DeviceInfo, VisitorInfo, Screenshot
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'img', 'created_at')  # Make sure 'created_at' is included here
+    search_fields = ('img',)
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Location)
+admin.site.register(DeviceInfo)
+admin.site.register(VisitorInfo)
+admin.site.register(Screenshot)
